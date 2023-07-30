46975a06689c1ee92046223befe445f41e4a1844
Danak-UY <37196682+Danak-UY@users.noreply.github.com>
2023-07-30 00:24:00 -0300

    feat: basic layout and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./index.html", "./js/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          "x-light": "#BDBDBD",
+          light: "#4f4f4f",
+          dark: "#333333",
+        },
+      },
+    },
+    fontFamily: {
+      sans: ['"Space mono"', "sans-serif"],
+    },
+    container: {
+      center: true,
+    },
   },
   plugins: [],
 };
